260f53e9a643f8be9154b50b72fdf1df9fad8f2f
RohanPhadnis <rohan.phadnis.usa@gmail.com>
2024-07-11 09:54:04 -0700

    parsing CL args

--- mft-fs/main/main.go
+++ mft-fs/main/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"flag"
+	"fmt"
 	"log"
 	"os"
 
@@ -13,9 +15,15 @@ import (
 
 func main() {
 
+	// reading commandline args
+	mountDirectory := flag.String("mountDirectory", "", "mount directory")
+	rootDirectory := flag.String("rootDirectory", "", "root directory")
+	flag.Parse()
+
+	fmt.Println(fmt.Sprintf("mountDirectory: %s, rootDirectory: %s", *mountDirectory, *rootDirectory))
 	// create an appropriate file system
 	// printer("started")
-	fs, _ := basicfs.NewBasicFS("./test")
+	fs, _ := basicfs.NewBasicFS(*rootDirectory)
 	server := fuseutil.NewFileSystemServer(&fs)
 
 	// mount the filesystem
@@ -24,7 +32,7 @@ func main() {
 		DebugLogger: log.New(os.Stderr, "fuse: ", 0),
 		ErrorLogger: log.New(os.Stderr, "fuse: ", 0),
 	}
-	mfs, err := fuse.Mount("./mount", server, cfg)
+	mfs, err := fuse.Mount(*mountDirectory, server, cfg)
 	if err != nil {
 		log.Fatalf("Mount: %v", err)
 	}
